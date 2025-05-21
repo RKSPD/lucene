59484a153c322bb59ae104c44b91e273734e6e69
rikhil konduru <rikhil@amazon.com>
2025-05-21 12:15:13 -0700

    Updated JVectorCodec and JVectorFormat to almost final versions, almost direct port

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/codecs/jvector/JVectorCodec.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/codecs/jvector/JVectorCodec.java
@@ -1,18 +1,41 @@
 package org.apache.lucene.sandbox.codecs.jvector;
 
+import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.FilterCodec;
 import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.lucene103.Lucene103Codec;
 
+//public class JVectorCodec extends FilterCodec {
+//    private final KnnVectorsFormat knnFormat = new JVectorFormat();
+//
+//    public JVectorCodec() {
+//        super("JVector", new Lucene103Codec());
+//    }
+//
+//    @Override
+//    public KnnVectorsFormat knnVectorsFormat() {
+//        return knnFormat;
+//    }
+//}
+
 public class JVectorCodec extends FilterCodec {
-    private final KnnVectorsFormat knnFormat = new JVectorFormat();
+
+    public static final String CODEC_NAME = "JVectorCodec";
+    private int minBatchSizeForQuantization;
 
     public JVectorCodec() {
-        super("JVector", new Lucene103Codec());
+        this(CODEC_NAME, new Lucene103Codec(), JVectorFormat.DEFAULT_MINIMUM_BATCH_SIZE_FOR_QUANTIZATION);
+    }
+
+    public JVectorCodec(int minBatchSizeForQuantization) {
+        this(CODEC_NAME, new Lucene103Codec(), minBatchSizeForQuantization);
+    }
+
+    public JVectorCodec(String codecName, Codec delegate, int minBatchSizeForQuantization) {
+        super(codecName, delegate);
+        this.minBatchSizeForQuantization = minBatchSizeForQuantization;
     }
 
     @Override
-    public KnnVectorsFormat knnVectorsFormat() {
-        return knnFormat;
-    }
+    public KnnVectorsFormat knnVectorsFormat() { return new JVectorFormat(minBatchSizeForQuantization);}
 }
--- lucene/sandbox/src/java/org/apache/lucene/sandbox/codecs/jvector/JVectorFormat.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/codecs/jvector/JVectorFormat.java
@@ -6,13 +6,69 @@ import org.apache.lucene.codecs.KnnVectorsWriter;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
 
-
 import java.io.IOException;
 
+//public class JVectorFormat extends KnnVectorsFormat {
+//
+//    public static final int DEFAULT_MINIMUM_BATCH_SIZE_FOR_QUANTIZATION = 1024;
+//    public JVectorFormat() {
+//        super("JVectorFormat");
+//    }
+//
+//    @Override
+//    public KnnVectorsWriter fieldsWriter(SegmentWriteState state) throws IOException {
+//        return new JVectorWriter(state);
+//    }
+//
+//    @Override
+//    public KnnVectorsReader fieldsReader(SegmentReadState state) throws IOException {
+//        return new JVectorReader(state);
+//    }
+//
+//    @Override
+//    public int getMaxDimensions(String dim) {
+//        return 2048;
+//    }
+//}
+
+
 public class JVectorFormat extends KnnVectorsFormat {
+    public static final String NAME = "JVectorFormat";
+    public static final String META_CODEC_NAME = "JVectorVectorsFormatMeta";
+    public static final String VECTOR_INDEX_CODEC_NAME = "JVectorVectorsFormatIndex";
+    public static final String JVECTOR_FILES_SUFFIX = "jvector";
+    public static final String META_EXTENSION = "meta-" + JVECTOR_FILES_SUFFIX;
+    public static final String VECTOR_INDEX_EXTENSION = "data-" + JVECTOR_FILES_SUFFIX;
+    public static final int DEFAULT_MINIMUM_BATCH_SIZE_FOR_QUANTIZATION = 1024; // The minimum number of vectors required to trigger
+    // quantization
+    public static final int VERSION_START = 0;
+    public static final int VERSION_CURRENT = VERSION_START;
+    private static final int DEFAULT_MAX_CONN = 32;
+    private static final int DEFAULT_BEAM_WIDTH = 100;
+    private static final float DEFAULT_DEGREE_OVERFLOW = 1.2f;
+    private static final float DEFAULT_ALPHA = 1.2f;
+
+    private final int maxConn;
+    private final int beamWidth;
+    private final int minBatchSizeForQuantization;
 
     public JVectorFormat() {
-        super("JVectorFormat");
+        this(NAME, DEFAULT_MAX_CONN, DEFAULT_BEAM_WIDTH, DEFAULT_MINIMUM_BATCH_SIZE_FOR_QUANTIZATION);
+    }
+
+    public JVectorFormat(int minBatchSizeForQuantization) {
+        this(NAME, DEFAULT_MAX_CONN, DEFAULT_BEAM_WIDTH, minBatchSizeForQuantization);
+    }
+
+    public JVectorFormat(int maxConn, int beamWidth) {
+        this(NAME, maxConn, beamWidth, DEFAULT_MINIMUM_BATCH_SIZE_FOR_QUANTIZATION);
+    }
+
+    public JVectorFormat(String name, int maxConn, int beamWidth, int minBatchSizeForQuantization) {
+        super(name);
+        this.maxConn = maxConn;
+        this.beamWidth = beamWidth;
+        this.minBatchSizeForQuantization = minBatchSizeForQuantization;
     }
 
     @Override
@@ -27,6 +83,6 @@ public class JVectorFormat extends KnnVectorsFormat {
 
     @Override
     public int getMaxDimensions(String dim) {
-        return 2048;
+        return 8192;
     }
 }
